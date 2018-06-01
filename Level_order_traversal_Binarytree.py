16b54ebb0c8ea5d99ee718ee720eac60dbf4109f
Snigdha Purohit <srpurohit753@gmail.com>
2018-05-31 20:37:51 -0400

    Update Level_order_traversal_Binarytree.py

--- Level_order_traversal_Binarytree.py
+++ Level_order_traversal_Binarytree.py
@@ -55,3 +55,14 @@ def BFS(root):
             result.append(v[::-1])
         
     return result
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+print(BFS(root))
